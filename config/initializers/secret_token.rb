f999bdd2d2facd59bdd2352c5cbdc65822282bd3
moro <moronatural@gmail.com>
2011-11-17 18:43:26 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Copipedential::Application.config.secret_token = '7a8f7834949c50c4779716c761b043544ff806b2e1e25189f92480204785c14f2e006e91ffa827fc9a465d2c9baaf9592133df819e9dcd5af9a768023c67c817'
