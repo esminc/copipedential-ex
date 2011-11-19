756056ad6b9795a1b6bc90a4a04aafa35f1395c5
moro <moronatural@gmail.com>
2011-11-19 11:28:46 +0900

    cosmetic

--- app/helpers/snippets_helper.rb
+++ app/helpers/snippets_helper.rb
@@ -1,6 +1,6 @@
 module SnippetsHelper
   def syntax(snippet, limit = nil)
     body = limit ? snippet.body.lines.first(limit).join : snippet.body
-    CodeRay.scan(body, snippet.assumed_filetype.to_sym).div(line_numbers: :table)
+    CodeRay.scan(body, snippet.assumed_filetype.to_sym).div(line_numbers: :table, css: :style)
   end
 end
