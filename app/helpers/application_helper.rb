735d32e401789fd129640fbee29f7e2874e8aeac
moro <moronatural@gmail.com>
2011-11-21 18:56:06 +0900

    userを一覧で表示するよ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
   def user_mini_link(user, size = 24)
-    gravatar = "#{request.protocol}www.gravatar.com/avatar/#{user.gravatar}?s=#{size}"
-    link_to user.nickname, user, style: "background-image: url(#{gravatar})", class:'user-mini'
+    g = gravatar(user, size)
+    link_to user.nickname, user, style: "background-image: url(#{g})", class:'user-mini'
+  end
+
+  def gravatar(user, size)
+    "#{request.protocol}www.gravatar.com/avatar/#{user.gravatar}?s=#{size}"
   end
 
   def with_paging(objects, &block)
