8e848d1e638c6392e391adfd4e4fb24ee173a636
moro <moronatural@gmail.com>
2011-12-02 16:53:37 +0900

    refactor

--- app/models/hook.rb
+++ app/models/hook.rb
@@ -17,32 +17,7 @@ class Hook
     return unless enabled?
   end
 
-  def self.enabled?; !!ENV['ENDPOINT'] ; end
-
-  class Message
-    include Rails.application.routes.url_helpers
-
-    self.default_url_options = {protocol: 'https', host: 'copipedential.herokuapp.com'}
-
-    def initialize(snippet)
-      @snippet = snippet
-    end
-
-    def mention
-      verb = @snippet.id_was.nil? ? 'pasted' : 'updated'
-      receivers = @snippet.mentioneds.map(&:nickname).sort.join(' ')
-
-      "#{receivers}: #{author_nickname} #{verb} snippet for you -- #{permalink}"
-    end
-
-    private
-
-    def author_nickname
-      @snippet.author.nickname
-    end
-
-    def permalink
-      url_for(@snippet)
-    end
+  def self.enabled?
+    !!ENV['ENDPOINT']
   end
 end
