33bec9e1f17aea21e98dae7a5b847918f27cbdeb
moro <moronatural@gmail.com>
2011-12-02 18:23:59 +0900

    execute hook!

--- app/models/hook.rb
+++ app/models/hook.rb
@@ -1,4 +1,6 @@
-class Hook
+require 'net/https'
+
+class Hook < ActiveRecord::Base
   module MentionHook
     extend ActiveSupport::Concern
     included do
@@ -9,15 +11,42 @@ class Hook
     end
   end
 
+  BACKENDS = {
+    # TODO test
+    webpost: ->(message, config) {
+      uri = URI(config[:endpoint])
+      http = Net::HTTP.new(uri.host, uri.port)
+      if uri.scheme == 'https'
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE # FIXME
+      end
+
+      req = Net::HTTP::Post.new(uri.request_uri)
+      req.basic_auth(uri.user, uri.password) if uri.user
+      req.set_form_data(config[:post_key] => message)
+      http.start{|con| con.request(req) }
+    }
+  }
+
   def self.mention(snippet)
     hook(Message.new(snippet).mention)
   end
 
   def self.hook(message)
-    return unless enabled?
+    active.each {|hook| hook.hook(message) }
   end
 
-  def self.enabled?
-    !!ENV['ENDPOINT']
+  validates :name, presence: true
+  validates :engine, presence: true
+
+  scope :active, where(active: true)
+  serialize :config
+
+  def hook(message)
+    engine.try(:call, message, config)
+  end
+
+  def engine
+    BACKENDS[backend.to_sym]
   end
 end
--- spec/models/hook_spec.rb
+++ spec/models/hook_spec.rb
@@ -5,6 +5,36 @@ describe Hook do
     Hook::Message.default_url_options = {host: 'copipedential.example.com', protocol: 'https'}
   end
 
+  describe 'serialize and de-' do
+    let(:hook) { Hook.new(name: 'hi', backend: 'webpost') }
+    subject { Hook.find(hook.id).config } # deep reload of hook
+    before do
+      hook.config = {endpoint: 'https://api.example.com/foo', post_key: 'message'}
+      hook.save!
+    end
+
+    its([:endpoint]) { should ==  'https://api.example.com/foo' }
+  end
+
+  describe 'call hook-engine' do
+    let!(:spy) { [] }
+    let(:hook) { Hook.new(name: 'test', backend: 'spy', config: {hi: 'you'}) }
+
+    subject &:spy
+
+    before do
+      Hook::BACKENDS[:spy] = ->(message, config) { (spy << message) << config }
+      hook.save!
+      hook.hook('hello world')
+    end
+
+    after do
+      Hook::BACKENDS.delete(:spy)
+    end
+
+    it { should == ['hello world', {hi: 'you'}] }
+  end
+
   describe 'Message' do
     describe 'send a valid notification to endpoint' do
       include_context 'a [user] will paste a [snippet] w/ mentioning to [mentioned]'
